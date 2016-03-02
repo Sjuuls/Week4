41f9da1d65a91f321f312953a25685f91eb21a38
Martijn Veenvliet <ffw8e@hotmail.com>
2016-03-02 14:51:15 +0100

    Backend

--- backend/src/main/java/com/example/week4/backend/MyServlet.java
+++ backend/src/main/java/com/example/week4/backend/MyServlet.java
@@ -7,24 +7,67 @@
 package com.example.week4.backend;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyServlet extends HttpServlet {
+
+    private ArrayList<Persoon> personen;
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Please use the form to POST to this url");
+
+            return;
     }
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        String name = req.getParameter("name");
+        String sNaam = req.getParameter("naam");
+        Integer iLeeftijd =  Integer.parseInt(req.getParameter("leeftijd"));
+
         resp.setContentType("text/plain");
-        if(name == null) {
-            resp.getWriter().println("Please enter a name");
+        if(sNaam == null) {
+            resp.getWriter().println("Vul een naam in");
+        }if(iLeeftijd == 0 || iLeeftijd == null){
+            resp.getWriter().println("Vul de leeftijd in");
         }
-        resp.getWriter().println("Hello " + name);
+
+        Persoon oudstePersoon = getOudsePersoon();
+
+        resp.getWriter().println("De oudste persoon is " + oudstePersoon.Leeftijd + "jaar en zijn/haar naam is: " + oudstePersoon.Naam);
+    }
+    private void addPersoon(String sNaam, Integer iLeeftijd){
+        Persoon p = new Persoon();
+        p.Leeftijd = iLeeftijd;
+        p.Naam = sNaam;
+
+        personen.add(p);
+    }
+    private Persoon getOudsePersoon(){
+        Persoon resultPersoon = null;
+        Persoon MyPersoon = null;
+
+        for (int i = 0; i < personen.size(); i++){
+            MyPersoon = personen.get(i);
+
+            if(resultPersoon == null){
+                resultPersoon = MyPersoon;
+            }
+
+            if(MyPersoon.Leeftijd > resultPersoon.Leeftijd){
+                resultPersoon = MyPersoon;
+            }
+        }
+
+        return resultPersoon;
+    }
+
+    class Persoon{
+        public String Naam;
+        public Integer Leeftijd;
     }
 }
